83587ee4e895e018948bac5cceea52361b6f48a3
LeandroNeto <leandrohock@gmail.com>
2022-09-12 07:15:49 -0300

    Componente Pagination e Form de usuário

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Pagination } from "../../components/Form/Pagination";
 
 import { Sidebar } from "../../components/Form/Sidebar";
 import { Header } from "../../components/Header";
@@ -116,6 +117,7 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
